a3cd29638310844596b7315dd646ee078913bbdc
Jonas Karuzas <jonas.karuzas@gmail.com>
2020-11-17 23:50:42 +0200

    prideta komponentu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+
+const particleOptions = {
+  "particles": {
+      "number": {
+          "value": 50
+      },
+      "size": {
+          "value": 3
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "repulse"
+          }
+      }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particleOptions}/>
       <Navigation />
       <Logo />
-{/* {      <ImageLinkForm />
-      <FaceRecognition />} */}
+      <Rank />
+      <ImageLinkForm />
+{/* {      <FaceRecognition />} */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+   return (
+    <div>
+        <p className='white f3'>
+            {'Hei, your rank is ...'}
+        </p>
+        <p className='white f1'>
+            {'#2'}
+        </p>
+    </div>
+   )
+}
+
+export default Rank;
