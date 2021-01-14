014ae6c739cca9a80add987cf8d4d0dc3abcc9cd
Jonas Karuzas <jonas.karuzas@gmail.com>
2021-01-14 09:22:54 +0200

    Register and Signin Update

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,33 +1,88 @@
+import { render } from '@testing-library/react'
 import React from 'react'
 
-const Register = ({onRouteChange}) => {
-    return (
-    <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
-        <main className="pa4 black-80">
-            <form className="measure center">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Register</legend>
-                <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlFor="Name">Name</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="Name"  id="name" />
-                </div>
-                <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-                </div>
-                <div className="mv3">
-                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                </div>
-                </fieldset>
-                <div className="">
-                <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" onClick={() => {onRouteChange('home')}} />
-                </div>
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+    }
 
-            </form>
-        </main>
-    </article>
-    )
+    onNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3030/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data === 'success') {
+                this.props.onRouteChange('home');
+                console.log("kuku");
+            }
+        })
+    }
+
+
+
+    render() {
+          return (
+            <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+                <main className="pa4 black-80">
+                    <form className="measure center">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="Name">Name</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                            type="text" name="Name"  id="name" 
+                            onChange={this.onEmailChange}/>
+                        </div>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" name="email-address"  id="email-address" 
+                            onChange={this.onEmailChange}/>
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="password" name="password"  id="password" 
+                            onChange={this.onPasswordChange}/>
+                        </div>
+                        </fieldset>
+                        <div className="">
+                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                        type="submit" value="Register" 
+                        onClick={this.onSubmitSignIn} />
+                        </div>
+
+                    </form>
+                </main>
+            </article>
+
+        );
+    }
 }
+
 
 export default Register
